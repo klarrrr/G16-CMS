9e8c7ae2f355d91ec2a99d75ed2e9181f1a03ce5
De Padua, Charles Jeramy C. <cjdp.pogi@gmail.com>
2025-04-15 12:40:51 +0800

    Side Bar

---
+++ Mockup/index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>CMS Editor</title>
+    <link href="https://fonts.googleapis.com/css2?family=Nanum+Gothic:wght@400;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+    <aside>
+        <img src="lundayan_logo" alt="Lundayan Logo">
+    
+        <div class="menu section">
+            <div class="User Menu">
+                <h3>User Menu</h3>
+                <ul>           
+                    <li><a href="#"><i class="fas fa-bullhorn"></i> Announcements</a></li>
+                    <li><a href="#"><i class="fas fa-newspaper"></i> News & Articles</a></li>
+                    <li><a href=""><i class="fas fa-photo-video"></i> Multimedia</a></li>
+                    <li><a href=""><i class="fas fa-info-circle"></i> About us</a></li>
+                    <li><a href=""><i class="fas fa-envelope"></i> Email Inquiry</a></li>
+                    <li><a href=""><i class="fas fa-archive"></i> Archive</a></li>
+                </ul>
+            </div>
+        
+            <div class="Admin Menu">
+                <h3>Admin Menu</h3>
+                <ul>           
+                    <li><a href=""><i class="fas fa-chart-line"></i> Dashboard</a></li>
+                    <li><a href=""><i class="fas fa-file-alt"></i> Reports</a></li>
+                    <li><a href=""><i class="fas fa-users-cog"></i> User Management</a></li>
+                </ul>
+            </div>
+        </div>
+        
+        <div class="bottom links">
+            <ul>
+                <li class="settings"><a href="#"><i class="fas fa-cog"></i> Settings</a></li>
+                <li class="exit"><a href="#"><i class="fas fa-sign-out-alt"></i> Exit</a></li>
+            </ul>
+        </div>
+        
+    </aside>
+    
+    
+
+
+<main>
+ 
+</main>
+
+</body>
+</html>
