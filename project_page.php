4cfc4537c7aaa016b9813afec56241e7d2afb1bb
Klarenz Cobie O. Manrique <klarenzcobie99@gmail.com>
2025-04-15 21:15:40 +0800

    Initial Form of Project Page

---
+++ project_page.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Project Page</title>
+    <style>
+        form {
+            display: flex;
+            flex-direction: row;
+            gap: 50px;
+        }
+
+        form div {
+            border: 2px solid black;
+            padding: 50px;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Select Your Projects</h1>
+    <form action="" method="post">
+        <!-- TODO : Make this dynamic -->
+        <!-- Grab whatever the user has created -->
+        <div class="card1">
+            <h2>Card 1</h2>
+            <button>Options</button>
+        </div>
+        <div class="card2">
+            <h2>Card 2</h2>
+            <button>Options</button>
+        </div>
+        <div class="card3">
+            <h2>Card 3</h2>
+            <button>Options</button>
+        </div>
+    </form>
+</body>
+
+</html>
