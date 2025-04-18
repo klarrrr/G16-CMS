02eacc10dbf4b3a2989bfd239596211be5aea696
Klarenz Cobie O. Manrique <klarenzcobie99@gmail.com>
2025-04-18 12:31:11 +0800

    Finished all draft layout

--- admin_manage_user.php
+++ admin_manage_user.php
@@ -4,7 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Manage Users</title>
+    <link rel="stylesheet" href="styles_admin_manage_users.css">
 </head>
 
 <body>
