f107a54f6c31cbef80c8ece7842b12ec51d4d699
Klarenz Cobie O. Manrique <klarenzcobie99@gmail.com>
2025-04-20 21:30:39 +0800

    Layout Design

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Content Management System</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+    <div class="nav-container">
+        <nav>
+            <ul>
+                <li><a href=""><img src="svg/menu-svgrepo-com.svg" alt=""></a></li>
+                <li>
+                    <h1>contento</h1>
+                </li>
+                <li><a href=""><img src="svg/circle-user-svgrepo-com.svg" alt=""></a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="second-nav-container">
+        <nav>
+            <ul>
+                <li><a href="">Pages</a></li>
+                <li><a href="">Design</a></li>
+                <li><a href="">Settings</a></li>
+                <li><a href="">Domain</a></li>
+            </ul>
+        </nav>
+    </div>
+    <div class="main-page">
+        <div class="edit-page-details">
+            <div class="choose-page-container">
+            </div>
+            <div class="page-details-container"></div>
+        </div>
+        <div class="preview-site">
+            <iframe src="example.php" frameborder="0"></iframe>
+        </div>
+    </div>
+</body>
+
+</html>
