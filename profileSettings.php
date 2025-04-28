05c2a995bda332af4a08cf87b099f36b540d772a
Francisco-PaulJustin <francisco_pauljustin@plpasig.edu.ph>
2025-04-28 14:20:36 +0800

    Changes

--- profileSettings.php
+++ profileSettings.php
@@ -12,13 +12,13 @@
     <!-- Navigation -->
     <?php include 'nav_dashboard.php' ?>
 
-    <!-- Body -->
+    <!-- Left Sidebar -->
     <div class="main-container">
         <div class="settings-choices">
             <h3>Profile Settings</h3>
             <ul>
-                <li>Personal Information</li>
-                <li>Username & Password</li>
+                <li><a href="profileSettings.php">Personal Information</a></li>
+                <li> <a href="login-info.php">Username & Password</a></li>
                 <li>Notification</li>
                 <li>Connected Accounts</li>
             </ul>
@@ -26,17 +26,22 @@
             <button>Log Out</button>
         </div>
 
+        <!-- Right Main Container -->
         <div class="my-profile">
+
+            <!-- Darkmode Toggle -->
             <div class="theme-toggle">
-                <button id="toggleTheme"><i class='bx bxs-moon'></i> Darkmode Toggle</button>
-            </div>
+                <div>
+                    <h3>My Profile</h3>
+                </div>
+                <div class="toggle">
+                    <button id="toggleTheme"><i class='bx bxs-moon'></i> Darkmode</button>
+                </div>
+             </div>
 
-            <div>
-                <h3>My Profile</h3>
-            </div>
-
+            <!-- Profile Picture -->
             <div class="profile-picture">
-                <input type="image" src="#" alt="Profile Picture Placeholder">
+                <input type="image" src="#" alt="">
                 
                 <label for="file-upload" class="custom-file-upload">Change Picture</label>
                 <input id="file-upload" type="file" style="display: none;">
@@ -44,17 +49,21 @@
                 <button><p>Delete Picture</p></button>
             </div>
 
+            <!-- Personal Infos -->
             <div class="perso-info">
                 <h5>Full Name</h5>
                 <p class="bio">Bio / Additional Description</p>
             </div>
 
             <div class="input-fields">
+                <p class="label last"></p>
                 <input type="text" placeholder="Last Name">
+                <p class="label last"></p>
                 <input type="text" placeholder="First Name">
                 <input type="email" placeholder="Email Address">
                 <input type="text" placeholder="Bio">
 
+                <!-- Save & Discard Buttons -->
                 <div class="save-buttons">
                     <input type="button" name="saveChanges" value="Save Changes">
                     <input type="button" name="discardChanges" value="Discard Changes">
