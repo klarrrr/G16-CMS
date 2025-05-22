9b2f4e23eadd106f000c3564d957e1e217a75454
De Padua, Charles Jeramy C. <cjdp.pogi@gmail.com>
2025-05-23 03:47:52 +0800

    Changed sign in and nav path

--- lundayan-site-nav.php
+++ lundayan-site-nav.php
@@ -1,7 +1,7 @@
 <header>
     <nav>
         <ul class="nav-links">
-            <li><a href="lundayan-site-home.php">Home</a></li>
+            <li><a href="index.php">Home</a></li>
             <li><a href="lundayan-site-archive.php">Archive</a></li>
             <li><a href="lundayan-site-calendar.php">Calendar</a></li>
             <li><a href="lundayan-site-contact.php">Contact</a></li>
