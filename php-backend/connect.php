2ad4a3f10cecee8e5475687dedefea9cbdf829b3
De Padua, Charles Jeramy C. <cjdp.pogi@gmail.com>
2025-05-23 03:52:14 +0800

    Changed database connection info

--- php-backend/connect.php
+++ php-backend/connect.php
@@ -1,9 +1,14 @@
 <?php
+ $host = "sql210.infinityfree.com";  
+$username = "if0_39052828";  
+$password = "Zl3Gg7qEWqhh"; 
+$dbname = "if0_39052828_cms_db";  
 
-// Localhost, usernam, pass, db_name
-$conn = mysqli_connect("localhost", "root", "", "cms_db");
+ $conn = mysqli_connect($host, $username, $password, $dbname);
 
-if ($conn == false) {
-    // stop then output connect error
-    die("Error: " . mysqli_connect_error());
+ if ($conn == false) {
+    die("Connection failed: " . mysqli_connect_error());
 }
+
+echo "Connected successfully!";
+?>
