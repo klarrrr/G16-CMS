1a2d4fc2c9a18204f94225c31b22f4d5dd7cb77b
De Padua, Charles Jeramy C. <cjdp.pogi@gmail.com>
2025-06-02 15:29:25 +0800

    Initial Hositng setup

--- php-backend/connect.php
+++ php-backend/connect.php
@@ -1,7 +1,7 @@
 <?php
 
 // Localhost, usernam, pass, db_name
-$conn = mysqli_connect("localhost", "root", "", "cms_db");
+$conn = mysqli_connect("sql210.infinityfree.com", "if0_39052828", "Zl3Gg7qEWqhh", "if0_39052828_cms_db");
 
 if ($conn == false) {
     // stop then output connect error
